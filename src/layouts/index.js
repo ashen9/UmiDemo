5705ba640ec7ea9b8a07ad9513839aecaeeb3fb9
ashen9 <247668045@qq.com>
2019-12-25 16:41:00 +0800

    create umi project

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,6 @@
 import styles from './index.css';
 
-function BasicLayout(props) {
+/*function BasicLayout(props) {
   return (
     <div className={styles.normal}>
       <h1 className={styles.title}>Yay! Welcome to umi!</h1>
@@ -9,4 +9,28 @@ function BasicLayout(props) {
   );
 }
 
-export default BasicLayout;
+export default BasicLayout;*/
+
+
+import { Component } from 'react';
+import withRouter from 'umi/withRouter';
+
+class Layout extends Component {
+  componentDidUpdate(prevProps) {
+    debugger;
+    console.log('componentDidUpdate');
+    if (this.props.location !== prevProps.location) {
+      window.scrollTo(0, 0);
+    }
+  }
+  render() {
+    return (
+      <div className={styles.normal}>
+          <h1 className={styles.title}>Yay! Welcome to umi!</h1>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default withRouter(Layout);
